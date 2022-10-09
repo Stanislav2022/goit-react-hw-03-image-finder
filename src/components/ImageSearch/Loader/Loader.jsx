408b2372571eedfe3660013a58c1a3e6f2f605cf
Stanislav2022 <tsv5@ukr.net>
2022-10-09 23:56:35 +0300

    1

---
+++ src/components/ImageSearch/Loader/Loader.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { ThreeDots } from 'react-loader-spinner';
+
+export default function Loader() {
+  return (
+    <div><ThreeDots color="red" width={200} /></div>
+  )
+}
