473be19b3577d5616af086de74bdcb26ee604fb6
Stanislav2022 <tsv5@ukr.net>
2022-10-09 17:07:49 +0300

    1

--- src/components/ImageSearch/ImageSearch.jsx
+++ src/components/ImageSearch/ImageSearch.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import Searchbar from './Searchbar/Searchbar'
+import React, { Component } from 'react'
 
-export default function ImageSearch() {
-  return (
-    <div>ImageSearch</div>
-  )
+
+export default class ImageSearch extends Component {
+    
+  render() {
+    return (
+        <div>
+            <Searchbar/>
+      </div>
+    )
+  }
 }
+
--- src/components/ImageSearch/Searchbar/Searchbar.jsx
+++ src/components/ImageSearch/Searchbar/Searchbar.jsx
@@ -2,6 +2,20 @@
 
 export default function Searchbar() {
   return (
-    <div>Searchbar</div>
+    <header class="searchbar">
+  <form class="form">
+    <button type="submit" class="button">
+      <span class="button-label">Search</span>
+    </button>
+
+    <input
+      class="input"
+      type="text"
+      autocomplete="off"
+      autofocus
+      placeholder="Search images and photos"
+    />
+  </form>
+</header>
   )
 }
