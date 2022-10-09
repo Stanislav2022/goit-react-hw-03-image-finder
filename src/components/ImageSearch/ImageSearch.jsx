2d8bcf1b59931676542a48e7f48a8315f9d326a1
Stanislav2022 <tsv5@ukr.net>
2022-10-09 22:15:23 +0300

    1

--- src/components/ImageSearch/ImageSearch.jsx
+++ src/components/ImageSearch/ImageSearch.jsx
@@ -1,13 +1,22 @@
 import Searchbar from './Searchbar/Searchbar'
 import React, { Component } from 'react'
+import ImageGallery from './ImageGallery/ImageGallery'
 
 
 export default class ImageSearch extends Component {
-    
+    state = {
+        page: 1,
+        per_page: 12,
+        id: " ",
+        webformatURL: " ",
+        largeImageURL: " ",
+    }
+
   render() {
     return (
         <div>
-            <Searchbar/>
+            <Searchbar />
+            <ImageGallery/>
       </div>
     )
   }
