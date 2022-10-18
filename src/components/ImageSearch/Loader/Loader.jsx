43f37fdcbbf04dd03066604a8ac8fa9dc3e57ff3
Stanislav2022 <tsv5@ukr.net>
2022-10-19 00:48:46 +0300

    1

--- src/components/ImageSearch/Loader/Loader.jsx
+++ src/components/ImageSearch/Loader/Loader.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { ThreeDots } from 'react-loader-spinner';
+import css from "./Loader.module.css"
 
 export default function Loader() {
   return (
-    <div><ThreeDots color="red" width={100} /></div>
+    <div className={css.loader}><ThreeDots color="red" width={100} /></div>
   )
 }
